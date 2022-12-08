ba23f00e696e016e199236f36b6e9848a671533b
jiang yadong <18827637106@163.com>
2022-12-08 14:44:05 +0800

    fix restart0

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,14 +2,12 @@
 import { SearchPlugin } from "vitepress-plugin-search";
 import { defineConfig } from "vite";
 
-
-//default options
-var options = {
-  previewLength: 62,
-  buttonLabel: "Search",
-  placeholder: "Search docs",
-};
-
 export default defineConfig({
-  plugins: [SearchPlugin(options)],
+  plugins: [
+    SearchPlugin({
+      placeholder: "Procure por algo!",
+      buttonLabel: "Procurar",
+      previewLength: 10,
+    }),
+  ],
 });
